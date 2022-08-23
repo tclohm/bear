6345e44942048a958bfe5f0a43642705149905f1
Taylor Lohman <taylor.lohman.tl@gmail.com>
2022-08-23 11:36:48 -0700

    eval boolean

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -13,12 +13,27 @@ func TestEvalIntegerExpression(t *testing.T) {
 		expected 	int64
 	}{
 		{"5", 5},
-		{"5", 5},
+		{"10", 10},
 	}
 
 	for _, test := range tests {
 		evaluated := testEval(test.input)
 		testIntegerObject(t, evaluated, test.expected)
+	}
+}
+
+func TestEvalBooleanExpression(t *testing.T) {
+	tests := []struct {
+		input 		string
+		expected 	bool
+	}{
+		{"true", true},
+		{"false", false},
+	}
+
+	for _, test := range tests {
+		evaluated := testEval(test.input)
+		testBooleanObject(t, evaluated, test.expected)
 	}
 }
 
@@ -39,6 +54,19 @@ func testIntegerObject(t *testing.T, obj object.Object, expected int64) bool {
 	}
 	if result.Value != expected {
 		t.Errorf("object has wrong value. got=%d, want=%d", result.Value, expected)
+		return false
+	}
+	return true
+}
+
+func testBooleanObject(t *testing.T, obj object.Object, expected bool) bool {
+	result, ok := obj.(*object.Boolean)
+	if !ok {
+		t.Errorf("object is not Boolean. got=%T (%+v)", obj, obj)
+		return false
+	}
+	if result.Value != expected {
+		t.Errorf("object has wrong value. got=%t, want=%t", result.Value, expected)
 		return false
 	}
 	return true
