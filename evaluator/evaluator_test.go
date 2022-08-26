4e9b91f95f0bab13ed24e626dd6449a44cf54982
Taylor Lohman <taylor.lohman.tl@gmail.com>
2022-08-26 14:59:17 -0700

    concat

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -175,6 +175,10 @@ func TestErrorHandling(t *testing.T) {
         	"foobar",
         	"identifier not found: foobar",
         },
+        {
+        	`"Hello" - "World"`,
+        	"unknown operator: STRING - STRING",
+        },
     }
 
     for _, test := range tests {
@@ -260,6 +264,19 @@ func TestStringLiteral(t *testing.T) {
 	}
 
 	if str.Value != "Hello World" {
+		t.Errorf("String has wrong value. got=%q", str.Value)
+	}
+}
+
+func TestStringConcatenation(t *testing.T) {
+	input := `"Hello" + " " + "World!"`
+	evaluated := testEval(input)
+	str, ok := evaluated.(*object.String)
+	if !ok {
+		t.Fatalf("object is not String. got=%T (%+v)", evaluated, evaluated)
+	}
+
+	if str.Value != "Hello World!" {
 		t.Errorf("String has wrong value. got=%q", str.Value)
 	}
 }
