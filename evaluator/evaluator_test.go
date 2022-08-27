28add42e7c3ae2a0e28d609c915febbd2a3a7f0c
Taylor Lohman <taylor.lohman.tl@gmail.com>
2022-08-27 08:07:52 -0700

    hash indexing

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -179,6 +179,10 @@ func TestErrorHandling(t *testing.T) {
         	`"Hello" - "World"`,
         	"unknown operator: STRING - STRING",
         },
+        {
+        	`{"name": "Monkey"}[fn(x) { x }];`,
+        	"unusable as hash key: FUNCTION",
+        },
     }
 
     for _, test := range tests {
@@ -396,6 +400,53 @@ func TestHashLiterals(t *testing.T) {
 		testIntegerObject(t, pair.Value, expectedValue)
 	}
 }
+
+func TestHashIndexExpression(t *testing.T) {
+	tests := []struct{
+		input string
+		expected interface{}
+	}{
+		{
+			`{"foo": 5}["foo"]`,
+			 5,
+		},
+		{
+			`{"foo": 5}["bar"]`,
+			nil, 
+		},
+        {
+			`let key = "foo"; {"foo": 5}[key]`,
+			5,
+		},
+		{
+			`{}["foo"]`,
+			nil,
+		},
+		{
+			`{5: 5}[5]`,
+			5,
+		},
+		{
+			`{true: 5}[true]`,
+			5,
+		},
+		{
+			`{false: 5}[false]`,
+			5,
+		},
+	}
+
+	for _, test := range tests {
+		evaluated := testEval(test.input)
+		integer, ok := test.expected.(int)
+		if ok {
+			testIntegerObject(t, evaluated, int64(integer))
+		} else {
+			testNullObject(t, evaluated)
+		}
+	}
+}
+
 
 // MARK: -- helpers
 func testEval(input string) object.Object {
