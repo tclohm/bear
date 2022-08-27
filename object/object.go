6200856c7f7fba32ad77ec46b4e72e0553d27e15
Taylor Lohman <taylor.lohman.tl@gmail.com>
2022-08-27 07:46:43 -0700

    hashing and working on indexing

--- object/object.go
+++ object/object.go
@@ -5,6 +5,7 @@ import (
 	"bytes"
 	"bear/ast"
 	"strings"
+	"hash/fnv"
 )
 
 const (
@@ -17,6 +18,7 @@ const (
 	STRING_OBJ 			= "STRING"
 	BUILTIN_OBJ  		= "BUILTIN"
 	ARRAY_OBJ 			= "ARRAY"
+	HASH_OBJ 			= "HASH"
 )
 
 type ObjectType string
@@ -121,4 +123,59 @@ func (self *Array) Inspect() string {
 	out.WriteString("]")
 
 	return out.String()
+}
+
+type HashKey struct {
+	Type ObjectType
+	Value uint64
+}
+
+func (self *Boolean) HashKey() HashKey {
+	var value uint64
+
+	if self.Value {
+		value = 1
+	} else {
+		value = 0
+	}
+
+	return HashKey{Type: self.Type(), Value: value}
+}
+
+func (self *Integer) HashKey() HashKey {
+	return HashKey{Type: self.Type(), Value: uint64(self.Value)}
+}
+
+func (self *String) HashKey() HashKey {
+	h := fnv.New64a()
+	h.Write([]byte(self.Value))
+	return HashKey{Type: self.Type(), Value: h.Sum64()}
+}
+
+type HashPair struct {
+	Key Object
+	Value Object
+}
+
+type Hash struct {
+	Pairs map[HashKey]HashPair
+}
+
+func (self *Hash) Type() ObjectType { return HASH_OBJ }
+func (self *Hash) Inspect() string {
+	var out bytes.Buffer
+
+	pairs := []string{}
+	for _, pair := range self.Pairs {
+		pairs = append(pairs, fmt.Sprintf("%s: %s", pair.Key.Inspect(), pair.Value.Inspect()))
+	}
+
+	out.WriteString("{")
+	out.WriteString(strings.Join(pairs, ", "))
+	out.WriteString("}")
+	return out.String()
+}
+
+type Hashable interface {
+	HashKey() HashKey
 }
