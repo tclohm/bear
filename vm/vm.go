cf96e8ee99155d3404675a822f93410ea1a46102
Taylor Lohman <taylor.lohman.tl@gmail.com>
2022-09-04 12:05:18 -0700

    bang & minus

--- vm/vm.go
+++ vm/vm.go
@@ -71,6 +71,16 @@ func (self *VM) Run() error {
 			if err != nil {
 				return err
 			}
+		case code.OpBang:
+			err := self.executeBangOperator()
+			if err != nil {
+				return err
+			}
+		case code.OpMinus:
+			err := self.executeMinusOperator()
+			if err != nil {
+				return err
+			}
 		}
 	}
 
@@ -173,4 +183,28 @@ func nativeBoolToBooleanObject(input bool) *object.Boolean {
 		return True
 	}
 	return False
+}
+
+func (self *VM) executeBangOperator() error {
+	operand := self.pop()
+
+	switch operand {
+	case True:
+		return self.push(False)
+	case False:
+		return self.push(True)
+	default:
+		return self.push(False)
+	}
+}
+
+func (self *VM) executeMinusOperator() error {
+	operand := self.pop()
+
+	if operand.Type() != object.INTEGER_OBJ {
+		return fmt.Errorf("unsupported type for negation: %s", operand.Type())
+	}
+
+	value := operand.(*object.Integer).Value
+	return self.push(&object.Integer{Value: -value})
 }
